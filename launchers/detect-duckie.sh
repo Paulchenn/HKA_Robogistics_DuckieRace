9437631ed393ccadb235e7c431647358fabfe492
IRAS Duckietown David <duckie.town@web.de>
2025-05-21 20:27:48 +0200

    Added launching of camera and duckie detetion node

--- launchers/detect-duckie.sh
+++ launchers/detect-duckie.sh
@@ -5,8 +5,11 @@ source /environment.sh
 # initialize launch file
 dt-launchfile-init
 
-# launch subscriber
-rosrun followlane detect_duckie_node.py
+# launch camera node
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/camera_reader_node.py" &
+
+# launch duckie detection node
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/detect_duckie_node.py"
 
 # wait for app to end
 dt-launchfile-join
