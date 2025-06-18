ef4d0b3f5343cd3eeade151e80a4a2076dfd4b5e
ilham-9-9 <ilham.hank9@gmail.com>
2025-06-18 20:00:39 +0200

    Update default.sh to enable additional nodes for duckie detection and bypass

--- launchers/default.sh
+++ launchers/default.sh
@@ -29,9 +29,13 @@ dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/switch_control_node.py" &
 
 dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/camera_reader_node.py" &
 
-dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/control_lane_node.py" &
+#dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/control_lane_node.py" &
 
-dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/detect_duckie_node.py"
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/detect_duckie_node.py" &
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/display_detectedDuckie_node.py" &
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/bypass_duckie_node.py"
+
+#dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/detect_bot_node.py"
 
 #dt-exec python3 "$DT_REPO_PATH/packages/testtim/my_script.py" 
 
