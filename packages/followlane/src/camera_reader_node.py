f97b8a8eb90c5c9261b5f054cd3b891317160b7d
Paulchenn <glpa1013@h-ka.com>
2025-06-19 13:28:33 +0200

    lanefollowing robust first standard

--- packages/followlane/src/camera_reader_node.py
+++ packages/followlane/src/camera_reader_node.py
@@ -5,14 +5,13 @@ import rospy
 import cv2
 import yaml
 import numpy as np
+import time
 from duckietown.dtros import DTROS, NodeType
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 from std_msgs.msg import Float64
-from std_msgs.msg import Bool
 
 class CameraReaderNode(DTROS):
-
     def __init__(self, node_name):
         super(CameraReaderNode, self).__init__(node_name=node_name, node_type=NodeType.VISUALIZATION)
 
@@ -20,222 +19,159 @@ class CameraReaderNode(DTROS):
         self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
         self._bridge = CvBridge()
         self._window = "camera-reader"
+        self._config_path = 'packages/followlane/config/detect_lane.yaml'
 
-        self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
-        self.pub_lane = rospy.Publisher(f"/{self._vehicle_name}/detect/lane", Float64, queue_size=1)
-        self.pub_redline = rospy.Publisher(f"/{self._vehicle_name}/stop_line_detected",  Bool, queue_size=1)
-
-        with open('packages/followlane/config/detect_lane.yaml','r') as f:
+        with open(self._config_path, 'r') as f:
             self.conf = yaml.safe_load(f)
 
-        self.target_x_buffers = {i: [] for i in range(5)}  # 5 Boxen (idx 0–4)
+        self.debug = self.conf.get('show_debug', False)
+        self.target_x_buffer = []
+        self.image = None
+
+        rospy.Subscriber(self._camera_topic, CompressedImage, self.image_callback)
+        self.pub_lane = rospy.Publisher(f"/{self._vehicle_name}/detect/lane", Float64, queue_size=1)
         rospy.on_shutdown(self.fnShutDown)
 
-    def create_polygon_offset(self, offset_y):
-        shrink = offset_y * 0.9  # je höher offset, desto schmaler die Box
+    def image_callback(self, msg):
+        self.image = self._bridge.compressed_imgmsg_to_cv2(msg)
+        self._timestamp = msg.header.stamp
+
+    def create_polygon(self):
         return np.array([[
-            [self.conf['lane_image']['top_left_x'] + shrink, self.conf['lane_image']['top_left_y'] - offset_y],
-            [self.conf['lane_image']['top_right_x'] - shrink, self.conf['lane_image']['top_right_y'] - offset_y],
-            [self.conf['lane_image']['bottom_right_x'] - shrink, self.conf['lane_image']['bottom_right_y'] - offset_y],
-            [self.conf['lane_image']['bottom_left_x'] + shrink, self.conf['lane_image']['bottom_left_y'] - offset_y],
+            [self.conf['lane_image']['top_left_x'], self.conf['lane_image']['top_left_y']],
+            [self.conf['lane_image']['top_right_x'], self.conf['lane_image']['top_right_y']],
+            [self.conf['lane_image']['bottom_right_x'], self.conf['lane_image']['bottom_right_y']],
+            [self.conf['lane_image']['bottom_left_x'], self.conf['lane_image']['bottom_left_y']],
         ]], dtype=np.int32)
 
-    # def compute_target_x_from_polygon(self, polygon, mask_white, mask_yellow, mask_red, image, idx):
-    #     mask_poly = np.zeros_like(mask_white)
-    #     cv2.fillPoly(mask_poly, polygon, 255)
-
-    #     mw = cv2.bitwise_and(mask_white, mask_poly)
-    #     my = cv2.bitwise_and(mask_yellow, mask_poly)
-    #     mr = cv2.bitwise_and(mask_red, mask_poly)
-
-    #     image[mw > 0] = (0, 255, 0)
-    #     image[my > 0] = (255, 0, 0)
-    #     image[mr > 0] = (0, 0, 255)
-
-    #     def get_lines(mask):
-    #         blurred = cv2.GaussianBlur(mask, (5, 5), 0)
-    #         edges = cv2.Canny(blurred, 50, 150)
-    #         return cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=15, minLineLength=10, maxLineGap=300)
-
-
-    #     lines_white = get_lines(mw)
-    #     lines_yellow = get_lines(my)
-    #     lines_red = get_lines(mr)
-
-    #     # Linien für Visualisierung einzeichnen
-    #     if lines_white is not None:
-    #         for x1, y1, x2, y2 in lines_white[:, 0]:
-    #             cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    #     if lines_yellow is not None:
-    #         for x1, y1, x2, y2 in lines_yellow[:, 0]:
-    #             cv2.line(image, (x1, y1), (x2, y2), (0, 255, 255), 2)
-
-    #     x_white = None
-    #     x_yellow = None
-
-    #     if lines_white is not None:
-    #         white_xs = [(x1 + x2) / 2 for [[x1, _, x2, _]] in lines_white if ((x1 + x2) / 2) >= 310]
-    #         if white_xs:
-    #             x_white = min(white_xs)
-
-    #     if lines_yellow is not None:
-    #         yellow_xs = [(x1 + x2) / 2 for [[x1, _, x2, _]] in lines_yellow if ((x1 + x2) / 2) <= 340]
-    #         if yellow_xs:
-    #             x_yellow = max(yellow_xs)
-
-    #     if x_white is not None and x_yellow is not None:
-    #         return (x_white + x_yellow + 120) / 2
-    #     elif x_yellow is not None:
-    #         return x_yellow + 260
-    #     elif x_white is not None:
-    #         offset = 180 - 4.17 * idx ** 1.4
-    #         return (x_white - offset)
-    #     else:
-    #         return None
-
-    def compute_target_x_from_polygon(self, polygon, mask_white, mask_yellow, mask_red, image, idx):
-        # Maske auf Polygonbereich begrenzen
+    def compute_target_x_from_polygon(self, polygon, mask_white, mask_yellow, image):
+        min_area = 400
         mask_poly = np.zeros_like(mask_white)
         cv2.fillPoly(mask_poly, polygon, 255)
-
-        # Masken im Polygonbereich isolieren
         mw = cv2.bitwise_and(mask_white, mask_poly)
         my = cv2.bitwise_and(mask_yellow, mask_poly)
-        mr = cv2.bitwise_and(mask_red, mask_poly)
 
-        # Farbige Darstellung im Bild (optional für Debug)
-        image[mw > 0] = (0, 255, 0)     # Grün für weiße Maske
-        image[my > 0] = (255, 0, 0)     # Blau für gelbe Maske
-        image[mr > 0] = (0, 0, 255)     # Rot für rote Maske
+        edges_white = cv2.Canny(cv2.GaussianBlur(mw, (5, 5), 0), 50, 150)
+        edges_yellow = cv2.Canny(cv2.GaussianBlur(my, (5, 5), 0), 50, 150)
 
-        # Linien erkennen mit Hough-Transformation
-        def get_lines(mask):
-            blurred = cv2.GaussianBlur(mask, (5, 5), 0)
-            edges = cv2.Canny(blurred, 50, 150)
-            return cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=15, minLineLength=10, maxLineGap=300)
+        if self.debug:
+            cv2.imshow("edges-white", edges_white)
+            cv2.imshow("edges-yellow", edges_yellow)
 
-        lines_white = get_lines(mw)
-        lines_yellow = get_lines(my)
-        lines_red = get_lines(mr)
+        contours_white, _ = cv2.findContours(edges_white, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_yellow, _ = cv2.findContours(edges_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Linien einzeichnen zur Visualisierung
-        if lines_white is not None:
-            for x1, y1, x2, y2 in lines_white[:, 0]:
-                cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)  # Rot für weiße Linien
+        leftmost_x = None
+        for i, cnt in enumerate(contours_white):
+            area = cv2.contourArea(cnt)
+            if area <= min_area:
+                if self.debug:
+                    rospy.loginfo(f"[Weiß] {i}: zu klein (Fläche = {area:.1f})")
+                continue
+            M = cv2.moments(cnt)
+            if M['m00'] == 0:
+                if self.debug:
+                    rospy.loginfo(f"[Weiß] {i}: Schwerpunkt nicht berechenbar (m00 = 0)")
+                continue
+            cx = int(M['m10'] / M['m00'])
+            if self.debug:
+                rospy.loginfo(f"[Weiß] {i}: Schwerpunkt X = {cx}, Fläche = {area:.1f}")
+            if leftmost_x is None or cx < leftmost_x:
+                leftmost_x = cx
+                cv2.drawContours(image, [cnt], -1, (0, 255, 0), 2)
 
-        if lines_yellow is not None:
-            for x1, y1, x2, y2 in lines_yellow[:, 0]:
-                cv2.line(image, (x1, y1), (x2, y2), (0, 255, 255), 2)  # Gelb für gelbe Linien
+        rightmost_x = None
+        for i, cnt in enumerate(contours_yellow):
+            area = cv2.contourArea(cnt)
+            if area <= min_area:
+                if self.debug:
+                    rospy.loginfo(f"[Gelb] {i}: zu klein (Fläche = {area:.1f})")
+                continue
+            M = cv2.moments(cnt)
+            if M['m00'] == 0:
+                if self.debug:
+                    rospy.loginfo(f"[Gelb] {i}: Schwerpunkt nicht berechenbar (m00 = 0)")
+                continue
+            cx = int(M['m10'] / M['m00'])
+            rospy.loginfo(f"[Gelb] {i}: Schwerpunkt X = {cx}, Fläche = {area:.1f}")
+            if rightmost_x is None or cx > rightmost_x:
+                rightmost_x = cx
+                cv2.drawContours(image, [cnt], -1, (0, 255, 255), 2)
+        if self.debug:
+            rospy.loginfo(f"[Auswertung] Weiß X: {leftmost_x}, Gelb X: {rightmost_x}")
 
-        if lines_red is not None:
-            rospy.loginfo("Rote Linie gefunden zum Linie malen")
-            for x1, y1, x2, y2 in lines_red[:, 0]:
-                cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 2)  # Weiß für rote Linien
-
-        # Berechnung der Zielposition aus weißen und gelben Linien
-        x_white = None
-        x_yellow = None
-
-        if lines_white is not None:
-            white_xs = [(x1 + x2) / 2 for [[x1, _, x2, _]] in lines_white if ((x1 + x2) / 2) >= 310]
-            if white_xs:
-                x_white = min(white_xs)
-
-        if lines_yellow is not None:
-            yellow_xs = [(x1 + x2) / 2 for [[x1, _, x2, _]] in lines_yellow if ((x1 + x2) / 2) <= 340]
-            if yellow_xs:
-                x_yellow = max(yellow_xs)
-
-        # Ziel-x berechnen basierend auf gefundenen Linien
-        if x_white is not None and x_yellow is not None:
-            return (x_white + x_yellow + 120) / 2
-        elif x_yellow is not None:
-            return x_yellow + 260
-        elif x_white is not None:
-            offset = 180 - 4.17 * idx ** 1.4
-            return (x_white - offset)
+        if leftmost_x is not None and rightmost_x is not None:
+            return ((leftmost_x + rightmost_x) / 2) - 30
+        elif rightmost_x is not None:
+            return rightmost_x + 170
+        elif leftmost_x is not None:
+            return leftmost_x - 230
         else:
             return None
 
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self.image is None:
+                rate.sleep()
+                continue
 
-    def callback(self, msg):
-        whl, whh = self.conf['white']['hl'], self.conf['white']['hh']
-        wsl, wsh = self.conf['white']['sl'], self.conf['white']['sh']
-        wvl, wvh = self.conf['white']['vl'], self.conf['white']['vh']
+            start = time.time()
+            image = self.image.copy()
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            wh = self.conf['white']
+            gh = self.conf['gelb']
 
-        ghl, ghh = self.conf['gelb']['hl'], self.conf['gelb']['hh']
-        gsl, gsh = self.conf['gelb']['sl'], self.conf['gelb']['sh']
-        gvl, gvh = self.conf['gelb']['vl'], self.conf['gelb']['vh']
+            mask_white = cv2.inRange(hsv, (wh['hl'], wh['sl'], wh['vl']), (wh['hh'], wh['sh'], wh['vh']))
+            mask_yellow = cv2.inRange(hsv, (gh['hl'], gh['sl'], gh['vl']), (gh['hh'], gh['sh'], gh['vh']))
 
-        rhl1, rhh1 = 0, 10
-        rhl2, rhh2 = 170,180
-        rsl,rsh = 100, 255
-        rvl, rvh = 100, 255
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (12, 12))
+            mask_white = cv2.morphologyEx(mask_white, cv2.MORPH_OPEN, kernel)
+            mask_white = cv2.morphologyEx(mask_white, cv2.MORPH_CLOSE, kernel)
+            mask_yellow = cv2.morphologyEx(mask_yellow, cv2.MORPH_OPEN, kernel)
+            mask_yellow = cv2.morphologyEx(mask_yellow, cv2.MORPH_CLOSE, kernel)
 
-        image = self._bridge.compressed_imgmsg_to_cv2(msg)
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            polygon = self.create_polygon()
+            target_x = self.compute_target_x_from_polygon(polygon, mask_white, mask_yellow, image)
 
-        mask_white = cv2.inRange(hsv, (whl, wsl, wvl), (whh, wsh, wvh))
-        mask_yellow = cv2.inRange(hsv, (ghl, gsl, gvl), (ghh, gsh, gvh))
-        mask_red1 = cv2.inRange(hsv, (rhl1,rsl,rvl), (rhh1, rsh, rvh))
-        mask_red2 = cv2.inRange(hsv, (rhl2,rsl,rvl), (rhh2, rsh, rvh))
-        mask_red = cv2.bitwise_or(mask_red1, mask_red2)
+            if target_x is not None:
+                self.target_x_buffer.append(target_x)
+                if len(self.target_x_buffer) > 2:
+                    self.target_x_buffer.pop(0)
 
-        red_pixels = cv2.countNonZero(mask_red)
-        threshold = 5000 #Schwellenwert für die rote Line
-        
-        if red_pixels > threshold:
-            rospy.loginfo("Rote Linie erkannt, stoppe den Duckiebot!")
-            self.pub_lane.publish(Float64(0)) #Geschwindigkeit auf 0 setzen
-            self.pub_redline.publish(Bool(True))
+                smoothed_x = int(np.mean(self.target_x_buffer))
+                target_y = image.shape[0] - 50
 
+            
+                cv2.circle(image, (smoothed_x, target_y), 6, (255, 0, 255), -1)
+                cv2.putText(image, "Target", (smoothed_x - 20, target_y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
-        cv2.imshow(self._window, image)
-        cv2.waitKey(1)
-
-        polygons = [self.create_polygon_offset(offset_y=i*40) for i in range(5)] #offset kann geändert werden
-
-        target_xs_raw = []
-
-        for idx, poly in enumerate(polygons):
-            tx = self.compute_target_x_from_polygon(poly, mask_white, mask_yellow, mask_red, image, idx=idx)
-            if tx is not None:
-                target_xs_raw.append((idx, tx))
-
-        for idx, (poly, tx_raw) in enumerate(zip(polygons, [tx for _, tx in target_xs_raw])):
-            self.target_x_buffers[idx].append(tx_raw)
-            if len(self.target_x_buffers[idx]) > 15:
-                self.target_x_buffers[idx].pop(0)
-
-            smoothed_x = int(np.mean(self.target_x_buffers[idx]))
-
-
-            # Y-Mitte der Polygonbox berechnen
-            y_coords = [point[1] for point in poly[0]]
-            target_y = int(sum(y_coords) / len(y_coords))
-
-            # Visualisierung
-            cv2.circle(image, (smoothed_x, target_y), 6, (255, 0, 255), -1)
-            cv2.putText(image, f"Target{idx+1}", (smoothed_x - 30, target_y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 255), 1)
-
-            if idx == 0:
                 self.pub_lane.publish(Float64(smoothed_x))
 
-        for poly in polygons:
-            cv2.polylines(image, poly, isClosed=True, color=(255, 255, 255), thickness=2)
+            # Mittelpunkt markieren (immer)
+            center_x = int(640 / 2)
+            center_y = image.shape[0] - 50  # gleiche Höhe wie Target
+            cv2.circle(image, (center_x, center_y), 6, (0, 0, 255), -1)
+            cv2.putText(image, "Center", (center_x - 25, center_y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            
+            cv2.polylines(image, polygon, isClosed=True, color=(255, 255, 255), thickness=2)
+            cv2.imshow(self._window, image)
+            cv2.waitKey(1)
 
-        cv2.imshow(self._window, image)
-        cv2.waitKey(1)
+            if self.debug:
+                rospy.loginfo(f"[Latenz] Jetzt: {rospy.Time.now().to_sec():.3f}, Bild-Zeitstempel: {self._timestamp.to_sec():.3f}")
+                rospy.loginfo(f"[Verzögerung] {(rospy.Time.now() - self._timestamp).to_sec():.3f} Sekunden")
+                rospy.loginfo(f"[Timer] Gesamtzeit: {(time.time() - start):.3f}s")
+
+            rate.sleep()
 
     def fnShutDown(self):
-        with open('packages/followlane/config/detect_lane.yaml','w') as f:
+        with open(self._config_path, 'w') as f:
             yaml.dump(self.conf, f)
         print("Config saved")
 
-def nothing(x): pass
-
 if __name__ == '__main__':
     node = CameraReaderNode(node_name='camera_reader_node')
-    rospy.spin()
+    node.run()
--- packages/followlane/src/control_lane_node.py
+++ packages/followlane/src/control_lane_node.py
@@ -1,95 +1,85 @@
 #!/usr/bin/env python3
 
 import rospy
+import os
+import yaml
 from std_msgs.msg import Float64, Int32
 from duckietown_msgs.msg import Twist2DStamped
-import os
 from duckietown.dtros import DTROS, NodeType
 from switch_control_node import ControlType
 
 class ControlLaneNode(DTROS):
     def __init__(self, node_name):
         super(ControlLaneNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        
-        self.enable = False
-        self._vehicle_name = os.environ['VEHICLE_NAME']
 
-        # Publisher an den car_cmd_switch_node
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        self.enable = False
+
+        # Konfig laden
+        self._config_path = 'packages/followlane/config/detect_lane.yaml'
+        with open(self._config_path, 'r') as f:
+            self.conf = yaml.safe_load(f)
+        self.v_min = self.conf.get("v_min", 0.1)
+        self.v_max = self.conf.get("v_max", 0.3)
+
+        # Publisher
         twist_topic = f"/{self._vehicle_name}/car_cmd_switch_node/cmd"
         self.pub_cmd_vel = rospy.Publisher(twist_topic, Twist2DStamped, queue_size=1)
 
-        # Subscriber für Zielspurpunkt (target_x) und Modus
-        self.sub_lane = rospy.Subscriber(f'/{self._vehicle_name}/detect/lane', Float64, self.cbFollowLane, queue_size=1)
+        # Subscriber
+        self.sub_lane = rospy.Subscriber(f"/{self._vehicle_name}/detect/lane", Float64, self.cbFollowLane, queue_size=1)
         self.sub_control = rospy.Subscriber(f"/{self._vehicle_name}/switch/control", Int32, self.cbControl, queue_size=1)
 
-        # PID-Parameter
-        self.kp = 7.5
-        self.ki = 0.01
-        self.kd = 0.3
-        self.alpha = 0.9  # Glättungsfaktor (0 = sehr glatt, 1 = keine Glättung)
+        # Minimaler PID
+        self.kp = 2.5
+        self.ki = 0.3
+        self.kd = 0.2
 
-        self.filtered_error = 0.0
         self.integral = 0.0
         self.last_error = 0.0
         self.last_time = rospy.Time.now()
-
-        # Debugging
-        self.print_counter = 0
 
         rospy.on_shutdown(self.fnShutDown)
 
     def cbControl(self, msg):
         self.enable = (msg.data == ControlType.Lane.value)
+        rospy.loginfo(f"[CONTROL] Lane following {'ENABLED' if self.enable else 'DISABLED'}")
 
     def cbFollowLane(self, desired_center):
         if not self.enable:
-            print("[INFO] Lane following not enabled")
             return
-
-        if self.print_counter % 100 == 0:
-            print(f"[INFO] Received target_x: {desired_center.data}")
 
         center = desired_center.data
         self.followLane(center)
 
     def followLane(self, center):
         image_center = 640 / 2
-        raw_error = (image_center - center) / image_center
+        error = (image_center - center) / image_center  # Normalisierter Fehler [-1, 1]
 
         current_time = rospy.Time.now()
         dt = (current_time - self.last_time).to_sec()
         self.last_time = current_time
 
-        # Glättung
-        self.filtered_error = self.alpha * raw_error + (1 - self.alpha) * self.filtered_error
-        error = self.filtered_error
-
-        # Integral-Anteil
+        # PID
         self.integral += error * dt
-
-        # Differential-Anteil
         derivative = (error - self.last_error) / dt if dt > 0 else 0.0
         self.last_error = error
 
-        # PID-Regler
         omega = self.kp * error + self.ki * self.integral + self.kd * derivative
-        omega = max(min(omega, 6.0), -6.0)
+        omega = max(min(omega, 5.0), -5.0)
 
-        #print(omega)
-
-        v = 0.25
-
-        self.print_counter += 1
-        if self.print_counter % 100 == 0:
-            print(f"[PID TEST] P={self.kp*error:.3f}, I={self.ki*self.integral:.3f}, D={self.kd*derivative:.3f}, omega={omega:.3f}, dt={dt:.3f}")
+        # Dynamische Geschwindigkeit: max bei error = 0, min bei |error| = 1
+        error_abs = min(abs(error), 1.0)
+        v = self.v_max - (self.v_max - self.v_min) * error_abs
 
         twist = Twist2DStamped(v=v, omega=omega)
         self.pub_cmd_vel.publish(twist)
 
-    def fnShutDown(self):
-        rospy.loginfo("Shutting down. Sending stop command...")
-        stop_msg = Twist2DStamped(v=0.0, omega=0.0)
+        rospy.loginfo(f"[PID] e={error:.3f}, P={self.kp * error:.3f}, I={self.ki * self.integral:.3f}, D={self.kd * derivative:.3f}, ω={omega:.3f}, v={v:.3f}")
 
+    def fnShutDown(self):
+        rospy.loginfo("[SHUTDOWN] Sending stop command...")
+        stop_msg = Twist2DStamped(v=0.0, omega=0.0)
         for _ in range(5):
             self.pub_cmd_vel.publish(stop_msg)
             rospy.sleep(0.1)
