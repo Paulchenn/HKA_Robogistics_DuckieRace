1f19ab10ac4fce983ff1f406a413828558da4d7f
ilham-9-9 <ilham.hank9@gmail.com>
2025-06-19 16:19:40 +0200

    Update build_and_run.sh to change runtime target from gustav to daffy

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dts devel build -f
-dts devel run -R gustav -X
+dts devel run -X -R daffy #-L show-raw-cam-img -v /home/duckie3/QuackSquad/HKA_Robogistics_DuckieRace/packages/dataset_botSlot:/home/QuackSquad/dataset_botSlot
