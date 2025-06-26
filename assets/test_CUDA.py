58839eb3cb6c55c38d410614ed737f94a36e932b
xrstu <xrstu@XRKR-26-XMG>
2025-06-26 23:01:48 +0200

    Add memory information output to CUDA test script

--- assets/test_CUDA.py
+++ assets/test_CUDA.py
@@ -4,3 +4,4 @@ print("CUDA available:", torch.cuda.is_available())
 print("CUDA device count:", torch.cuda.device_count())
 if torch.cuda.is_available():
 	print("CUDA device name:", torch.cuda.get_device_name(0))
+	print(torch.cuda.get_device_properties(0).total_memory / 1024**3, "GB")
