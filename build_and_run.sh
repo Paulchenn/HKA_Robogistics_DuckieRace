4e9796a6c516e2c4a03e49fd7a4be97d22720a70
Melina110399 <scme1025@h-ka.de>
2025-07-04 11:52:29 +0200

    Update build_and_run.sh to run with 'donald' and enable raw camera image display

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dts devel build -f
-dts devel run -X -R daffy #-L show-raw-cam-img -v /home/duckie3/QuackSquad/HKA_Robogistics_DuckieRace/packages/dataset_botSlot:/home/QuackSquad/dataset_botSlot
+dts devel run -X -R donald -L show-raw-cam-img #detect-object #-v /home/duckie3/QuackSquad/HKA_Robogistics_DuckieRace/packages/dataset_botSlot:/home/QuackSquad/dataset_botSlot
