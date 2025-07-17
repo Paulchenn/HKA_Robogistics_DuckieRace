daf805fc48c3f9fd82cb842226a164f9b68a7138
Paulchenn <glpa1013@h-ka.com>
2025-07-17 16:33:48 +0200

    New Training done and created parking node

--- launchers/detect-object.sh
+++ launchers/detect-object.sh
@@ -12,7 +12,10 @@ dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/camera_reader_node.py" &
 dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/detect_object_node.py" &
 
 # launch display node
-dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/display_yoloResult_node.py"
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/display_yoloResult_node.py" & 
+
+# launch parking node
+dt-exec python3 "$DT_REPO_PATH/packages/followlane/src/parking_node.py"
 
 # wait for app to end
 dt-launchfile-join
