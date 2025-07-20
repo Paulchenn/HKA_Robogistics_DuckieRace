5979ea615b0858fd37559fd36b8f8b765d766345
h13 <hauke.stuerenburg@gmail.com>
2025-07-20 19:04:59 +0200

    Changed to donald

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dts devel build -f
-dts devel run -X -R track -L default #detect-object #-v /home/duckie3/QuackSquad/HKA_Robogistics_DuckieRace/packages/dataset_botSlot:/home/QuackSquad/dataset_botSlot
+dts devel run -X -R donald -L default #calibration
+#detect-object #-v /home/duckie3/QuackSquad/HKA_Robogistics_DuckieRace/packages/dataset_botSlot:/home/QuackSquad/dataset_botSlot
